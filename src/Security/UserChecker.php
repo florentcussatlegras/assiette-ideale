d5e71de8334cd6ed7fc3568bf7db59020d30268a
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-03-17 09:52:48 +0100

    add UserChecker Service for non activated user

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,29 @@
+<?php
+// src/Security/UserChecker.php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getIsVerified()) {
+            throw new CustomUserMessageAccountStatusException(
+                "Votre compte n'est pas activé. Veuillez saisir le code de confirmation envoyé par email."
+            );
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+    }
+}
